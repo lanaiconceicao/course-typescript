9788109d3ebcb78f7d655fe6d6f871d3034771f6
Lanai Conceição <lanai.conceicao@mercadolivre.com>
2023-01-17 09:54:25 -0300

    feat(app): react variables with typescript

--- 11_react-ts/src/App.tsx
+++ 11_react-ts/src/App.tsx
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
+  // 1 - Variáveis
+  const name: string = "Milena";
+  const age: number = 28;
+  const isWorking: boolean = true;
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Typescript com React</h1>
+      <h2>Nome: {name}</h2>
+      <p>Idade: {age}</p>
+      {isWorking && (
+        <p>Está trabalhando</p>
+      )}
     </div>
   );
 }
