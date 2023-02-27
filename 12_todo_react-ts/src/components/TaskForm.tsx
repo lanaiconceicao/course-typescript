f5b8c3ac636c9017f538c8e6959da05da9f82852
Lanai Conceição <lanai.conceicao@mercadolivre.com>
2023-02-27 09:24:35 -0300

    feat(taskform): add styles to form

--- 12_todo_react-ts/src/components/TaskForm.tsx
+++ 12_todo_react-ts/src/components/TaskForm.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
 
+// CSS
+import style from './TaskForm.module.css';
+
 export interface IAppProps {
   btnText: string
 }
 
 export function TaskForm ({ btnText }: IAppProps) {
   return (
-    <form>
-      <div>
+    <form className={style.form}>
+      <div className={style.input_container}>
         <label htmlFor='title'>Título:</label>
         <input type='text' name='title' placeholder='Título da tarefa'/>
       </div>
-      <div>
+      <div className={style.input_container}>
         <label htmlFor='dificulty'>Dificuldade:</label>
         <input type='text' name='dificulty' placeholder='Dificuldade da tarefa'/>
       </div>
