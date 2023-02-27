3ea13e64ddd5657d22c43230bf09a91567e2c9c0
Lanai Conceição <lanai.conceicao@mercadolivre.com>
2023-02-27 11:25:30 -0300

    feat(tasklist): add task list and styles

--- 12_todo_react-ts/src/components/TaskList.tsx
+++ 12_todo_react-ts/src/components/TaskList.tsx
@@ -1,12 +1,34 @@
 import React from 'react';
 
+// interfaces
+import { ITask } from '../interfaces/Task';
+
+// CSS
+import styles from './TaskList.module.css'
+
 export interface IAppProps {
+  taskList: ITask[]
 }
 
-export function TaskList (props: IAppProps) {
+export function TaskList ({ taskList }: IAppProps) {
   return (
     <div>
-      lista de tarefas
+      {taskList.length > 0 ? (
+        taskList.map((task) => (
+          <div key={task.id} className={styles.task}>
+            <div className={styles.details}>
+              <h4>{task.title}</h4>
+              <p>Dificuldade: {task.difficulty}</p>
+            </div>
+            <div className={styles.actions}>
+              <i className='bi bi-pencil'></i>
+              <i className='bi bi-trash'></i>
+            </div>
+          </div>
+        ))
+      ) : (
+        <p>Não tem tarefas cadastradas</p>
+      )}
     </div>
   );
 }
