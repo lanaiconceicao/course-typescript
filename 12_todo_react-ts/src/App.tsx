fdcd6e321cc60aab6827e843623d455569ec03b0
Lanai Conceição <lanai.conceicao@mercadolivre.com>
2023-02-27 11:02:52 -0300

    feat(taskform, app): add addtaskhandler function, connect local state with inputs to create a task list

--- 12_todo_react-ts/src/App.tsx
+++ 12_todo_react-ts/src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 // components
 import { Footer } from './components/Footer';
@@ -13,13 +13,15 @@ import { TaskList } from './components/TaskList';
 import { ITask } from './interfaces/Task';
 
 function App() {
+  const [taskList, setTaskList] = useState<ITask[]>([])
+
   return (
     <div>
       <Header />
       <main className={style.main}>
         <div>
           <h2>O que você vai fazer?</h2>
-          <TaskForm btnText='Criar tarefa' />
+          <TaskForm btnText='Criar tarefa' taskList={taskList} setTaskList={setTaskList} />
         </div>
         <div>
           <h2>Suas tarefas:</h2>
--- 12_todo_react-ts/src/components/TaskForm.tsx
+++ 12_todo_react-ts/src/components/TaskForm.tsx
@@ -8,16 +8,27 @@ import { ITask } from '../interfaces/Task';
 
 export interface IAppProps {
   btnText: string
+  taskList: ITask[]
+  setTaskList?: React.Dispatch<React.SetStateAction<ITask[]>>
 }
 
-export function TaskForm ({ btnText }: IAppProps) {
+export function TaskForm ({ btnText, taskList, setTaskList }: IAppProps) {
   const [id, setId] = useState<number>(0)
   const [title, setTitle] = useState<string>('')
   const [difficulty, setDifficulty] = useState<number>(0)
 
-  const addTaskHandler = () => {
+  const addTaskHandler = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
 
+    const id = Math.floor(Math.random() * 1000);
+    const newTask: ITask = { id, title, difficulty };
+
+    setTaskList!([...taskList, newTask]);
+
+    setTitle('')
+    setDifficulty(0)
   }
+  
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     if(e.target.name === 'title') {
       setTitle(e.target.value)
@@ -30,11 +41,23 @@ export function TaskForm ({ btnText }: IAppProps) {
     <form onSubmit={addTaskHandler} className={style.form}>
       <div className={style.input_container}>
         <label htmlFor='title'>Título:</label>
-        <input type='text' name='title' placeholder='Título da tarefa' onChange={handleChange}/>
+        <input
+          type='text'
+          name='title'
+          placeholder='Título da tarefa'
+          onChange={handleChange}
+          value={title}
+        />
       </div>
       <div className={style.input_container}>
         <label htmlFor='dificulty'>Dificuldade:</label>
-        <input type='text' name='dificulty' placeholder='Dificuldade da tarefa' onChange={handleChange}/>
+        <input
+          type='text'
+          name='dificulty'
+          placeholder='Dificuldade da tarefa'
+          onChange={handleChange}
+          value={difficulty}
+        />
       </div>
       <input type='submit' value={btnText} />
     </form>
