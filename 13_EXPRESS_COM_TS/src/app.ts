c575567bdbdff91222d0655aefede9fc52b051a7
Lanai Conceição <lanai.conceicao@mercadolivre.com>
2023-03-01 09:52:50 -0300

    feat(express with typescript): initialize express

--- 13_EXPRESS_COM_TS/src/app.ts
+++ 13_EXPRESS_COM_TS/src/app.ts
@@ -1,2 +1,15 @@
 // 1 - iniciando projeto
 console.log("Express + TS !!!");
+
+// 2 - init express
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  return res.send("Hello Express!")
+});
+
+app.listen(3000, () => {
+  console.log("Aplicação de TS + Express funcionando!");
+});
