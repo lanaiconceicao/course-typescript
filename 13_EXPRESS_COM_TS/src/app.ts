355d17b39fbb4b501538f1f1f25b00b15f538331
Lanai Conceição <lanai.conceicao@mercadolivre.com>
2023-03-01 10:50:37 -0300

    feat(express with typescript): create a route more complex

--- 13_EXPRESS_COM_TS/src/app.ts
+++ 13_EXPRESS_COM_TS/src/app.ts
@@ -63,6 +63,16 @@ app.get("/api/product/:id", (req: Request, res: Response) => {
   }
 });
 
+// 8 - rotas complexas
+app.get("/api/product/:id/review/:reviewId", (req: Request, res: Response) => {
+  console.log(req.params);
+
+  const productId = req.params.id
+  const reviewId = req.params.reviewId
+
+  return res.send(`Acessando a review ${reviewId} do produto ${productId}`)
+});
+
 app.listen(3000, () => {
   console.log("Aplicação de TS + Express funcionando!");
 });
