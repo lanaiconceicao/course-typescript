4151a49a7804d015d04f1fc8d297f19bbb7a0080
Lanai Conceição <lanai.conceicao@mercadolivre.com>
2023-03-02 17:28:19 -0300

    feat(express with typescript): create error

--- 13_EXPRESS_COM_TS/src/app.ts
+++ 13_EXPRESS_COM_TS/src/app.ts
@@ -110,6 +110,16 @@ app.get("/api/user/:id/details/:name", (req: Request<{id: string, name: string}>
   return res.json({ status: true });
 });
 
+// 13 - tratando erros
+app.get("/api/error", (req: Request, res: Response) => {
+  try {
+    // a nossa lógica
+    throw new Error("Algo deu errado");
+  } catch (e: any) {
+    res.status(500).json({ msg: e.message });
+  }
+});
+
 
 app.listen(3000, () => {
   console.log("Aplicação de TS + Express funcionando!");
