c662d0fdd4e344641bb42fdbfc63c8614033d544
Lanai Conceição <lanai.conceicao@mercadolivre.com>
2023-03-02 17:14:23 -0300

    feat(express with typescript): create a router with middleware

--- 13_EXPRESS_COM_TS/src/app.ts
+++ 13_EXPRESS_COM_TS/src/app.ts
@@ -2,7 +2,7 @@
 console.log("Express + TS !!!");
 
 // 2 - init express
-import express, {Request, Response} from "express";
+import express, {NextFunction, Request, Response} from "express";
 
 const app = express();
 
@@ -80,6 +80,20 @@ function getUser(req: Request, res: Response) {
 }
 
 app.get("/api/user/:id", getUser);
+
+// 10 - middlewares
+function checkUser(req: Request, res: Response, next: NextFunction) {
+  if(req.params.id === "1") {
+    console.log("Pode seguir...");
+    next();
+  } else {
+    console.log("Acesso restrito!");
+  }
+}
+
+app.get("/api/user/:id/access", checkUser, (req: Request, res: Response) => {
+  return res.json({ msg: "Bem vindo a área administrativa!" })
+});
 
 
 app.listen(3000, () => {
