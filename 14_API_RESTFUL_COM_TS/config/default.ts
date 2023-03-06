f4fc6483c026dd5193f4da5e35c586c54fe97508
Lanai Conceição <lanai.conceicao@mercadolivre.com>
2023-03-06 11:48:37 -0300

    feat(logger, db, default, app): config winston

--- 14_API_RESTFUL_COM_TS/config/default.ts
+++ 14_API_RESTFUL_COM_TS/config/default.ts
@@ -3,5 +3,6 @@ const dbPass = process.env.DB_PASS;
 
 export default {
   port: 3000,
-  dbUrl: `mongodb+srv://${dbUser}:${dbPass}@cluster0.bh0iw7q.mongodb.net/?retryWrites=true&w=majority`
+  dbUrl: `mongodb+srv://${dbUser}:${dbPass}@cluster0.bh0iw7q.mongodb.net/?retryWrites=true&w=majority`,
+  env: "development"
 }
--- 14_API_RESTFUL_COM_TS/src/app.ts
+++ 14_API_RESTFUL_COM_TS/src/app.ts
@@ -15,6 +15,9 @@ import db from "../config/db";
 // Routes
 import router from "./router";
 
+// Logger
+import Logger from "../config/logger";
+
 app.use("/api/", router);
 
 // app port
@@ -22,5 +25,5 @@ const port = config.get<number>("port");
 
 app.listen(port, async () => {
   await db();
-  console.log(`A aplicação está funcionando na porta: ${port}`);
+  Logger.info(`A aplicação está funcionando na porta: ${port}`);
 });
