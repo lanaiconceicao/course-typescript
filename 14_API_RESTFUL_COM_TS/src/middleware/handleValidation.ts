77706a813aa9a0d81004ce9a78bcbbd5a85ec878
Lanai Conceição <lanai.conceicao@mercadolivre.com>
2023-03-06 16:35:17 -0300

    feat(router, handlevalidation): add middleware with error validation

---
+++ 14_API_RESTFUL_COM_TS/src/middleware/handleValidation.ts
@@ -0,0 +1,18 @@
+import { Request, Response, NextFunction } from "express";
+import { validationResult } from "express-validator";
+
+export const validate = (req: Request, res: Response, next: NextFunction) => {
+  const errors = validationResult(req);
+
+  if(errors.isEmpty()) {
+    return next();
+  }
+
+  const extractedErrors: object[] = [];
+
+  errors.array().map((e) => extractedErrors.push({[e.param]: e.msg}));
+
+  return res.status(422).json({
+    errors: extractedErrors,
+  });
+};
--- 14_API_RESTFUL_COM_TS/src/router.ts
+++ 14_API_RESTFUL_COM_TS/src/router.ts
@@ -1,10 +1,13 @@
 import { Router, Request, Response } from "express";
 import { createMovie } from "./controllers/movieControllers";
 
+// validations
+import { validate } from "./middleware/handleValidation";
+
 const router = Router()
 
 export default router
   .get("/test", (req: Request, res: Response) => {
     res.status(200).send("API working!!!")
   })
-  .post("/movie", createMovie);
+  .post("/movie", validate, createMovie);
